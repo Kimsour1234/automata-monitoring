60e547969bae712451c0349fd6784c7bbc53c2b0
Kimsour1234 <contact@automatainspiring.com>
2025-11-25 17:17:54 +0100

    Update monitoring.py

--- api/monitoring.py
+++ api/monitoring.py
@@ -1,1 +1,7 @@
+from http.server import BaseHTTPRequestHandler
 
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
